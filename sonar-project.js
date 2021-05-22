b3234d3b614777d6e752d606ed26940e0e26e9c0
shiva B <70433406+shivabaalireddipalli@users.noreply.github.com>
2021-05-22 18:13:27 +0530

    Update sonar-project.js
    password and credientals add

--- sonar-project.js
+++ sonar-project.js
@@ -1,13 +1,13 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://54.253.237.52:9000/',
+  serverUrl: 'http://13.232.29.110:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
 	    'sonar.login': 'admin',
-	    'sonar.password': 'admin',
+	    'sonar.password': 'shiva@123',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
